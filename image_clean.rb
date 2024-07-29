5ce7208dfdab09a0df8930374e3dfb45dd37f370
Pusher <spunkspunkik@gmail.com>
2024-07-29 20:04:49 +0300

    added minimagic background changer

---
+++ image_clean.rb
@@ -0,0 +1,72 @@
+require 'rmagick'
+include Magick
+require 'mini_magick'
+
+image = Magick::Image.read('cleaned_images/images.png').first
+image = Image.new(image.columns, image.rows)
+image.format = 'PNG'
+image.fuzz = '10%'
+background_color = image.background_color
+image.paint_transparent(background_color)
+image.background_color = 'white'
+image.write('cleaned_images/images2.png')
+
+# input_path = 'images/images.png'
+# output_path = 'images/images_c.png'
+
+# image = Image.read(input_path).first
+# mask = image.clone.negate
+# new_background = Image.new(image.columns, image.rows)
+# new_background.background_color = 'white'
+# new_background = new_background.matte_reset!
+# result = new_background.composite(image, CenterGravity, OverCompositeOp)
+# result.write('cleaned_images/rm2_images.png')
+
+# def change_background(image_path, output_path, new_background_color = 'white', fuzz_factor = 20)
+#   image = Image.read(image_path).first
+#   background_color = image.pixel_color(0, 0)
+#   fuzz = (QuantumRange * fuzz_factor / 100.0).round
+#   image.alpha(ActivateAlphaChannel)
+#   new_background = Image.new(image.columns, image.rows)
+#   new_background.background_color = new_background_color
+#   new_background.matte_reset!
+#   result = new_background.composite(image, CenterGravity, OverCompositeOp)
+#   result.write(output_path)
+# end
+
+# change_background(input_path, 'cleaned_images/rm3_images.png', 'white', 20)
+
+
+# image = MiniMagick::Image.open(input_path)
+# image.combine_options do |c|
+#   c.fuzz "20%"
+#   c.background "white"
+#   c.transparent "white"
+# end
+# new_background = MiniMagick::Image.open(input_path)
+# new_background.combine_options do |c|
+#   c.alpha "remove" 
+#   c.background "white"
+#   c.flatten
+# end
+# new_background.write("cleaned_images/mm1_images.png")
+
+# convert images.png -bordercolor white -border 1x1 -matte -fill none -fuzz 20% -draw 'matte 1,1 floodfill' -shave 1x1 white_floodfill.png
+# composite  -compose Dst_Over -tile pattern:checkerboard white_floodfill.png white_floodfill_check.png
+# convert white_floodfill.png -background white -flatten white_floodfill_check2.png
+
+
+# MiniMagick.composite do |composite|
+  
+#   composite << "cleaned_images/mm2_images.png"
+#   composite.compose "Dst_Over"    # OverCompositeOp
+#   composite << "-tile" << "pattern:checkerboard"
+#   composite << "cleaned_images/mm3_images.png"
+# end
+
+# MiniMagick.mogrify do |mogrify|
+#   mogrify.merge! ["-bordercolor", "white", "-border", "1x1", "-matte", "-fill", "none", "-fuzz", "20%", "-draw", "matte 1,1 floodfill", "-shave", "1x1", "-background", "white", "-flatten"]
+#   mogrify << "cleaned_images/*"
+# end
+
+
